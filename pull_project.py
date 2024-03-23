cd92f023103a1b381d3ac7e6eae97e59e0f85390
TTArmy <164367724+TTArmy@users.noreply.github.com>
2024-03-23 11:16:52 +0200

    Add files via upload

--- pull_project.py
+++ pull_project.py
@@ -3,7 +3,9 @@ import json
 import os
 from datetime import datetime
 
-def fetch_time_series_data(api_key, start_date, end_date, base_currency, target_currencies, resolution, amount, places, format_):
+
+def fetch_time_series_data(api_key, start_date, end_date, base_currency, target_currencies, resolution, amount, places,
+                           format_):
     conn = http.client.HTTPSConnection("api.fxratesapi.com")
     url = f"/timeseries?access_key={api_key}&start_date={start_date.strftime('%Y-%m-%d')}T00:00:00&end_date={end_date.strftime('%Y-%m-%d')}T00:00:00&base={base_currency}&currencies={target_currencies}&resolution={resolution}&amount={amount}&places={places}&format={format_}"
     conn.request("GET", url)
@@ -11,11 +13,18 @@ def fetch_time_series_data(api_key, start_date, end_date, base_currency, target_
     data = res.read().decode("utf-8")
     conn.close()
     return json.loads(data)
+
+
 def get_env_variable(var_name):
     value = os.environ.get(var_name)
     if value is None:
         raise ValueError(f"Environment variable '{var_name}' is not set.")
+    if var_name == "API_KEY":
+        masked_value = value[0] + '*' * (len(value) - 4) + value[-3] + value[-2] + value[-1]
+        return masked_value
     return value
+
+
 def get_user_input():
     api_key = os.environ.get("API_KEY")
     start_date_input = os.environ.get("START_DATE")
@@ -28,7 +37,9 @@ def get_user_input():
     amount = int(os.environ.get("AMOUNT"))
     places = int(os.environ.get("PLACES"))
     format_ = os.environ.get("FORMAT")
+
     return api_key, start_date, end_date, base_currency, target_currencies, resolution, amount, places, format_
+
 
 def save_time_series_data(data, base_currency, target_currency, start_date, end_date):
     directory = f"time_series_data/{base_currency}"
@@ -41,6 +52,7 @@ def save_time_series_data(data, base_currency, target_currency, start_date, end_
     data_str_keys = {str(key): value for key, value in data.items()}
     with open(filename, 'w') as file:
         json.dump(data_str_keys, file)
+
 
 def parse_time_series_data(time_series_data, start_date, end_date):
     base_currency = time_series_data.get('base')
@@ -63,11 +75,11 @@ def parse_time_series_data(time_series_data, start_date, end_date):
         sorted_data = dict(sorted(data.items()))
         save_time_series_data(sorted_data, base_currency, currency, start_date, end_date)
 
+
 if __name__ == "__main__":
     try:
-        api_key = get_env_variable("API_KEY")
-        start_date = get_env_variable("START_DATE")
-        end_date = get_env_variable("END_DATE")
+        masked_api_key = get_env_variable("API_KEY")
+        start_date, end_date = get_user_input()[1:3]
         base_currency = get_env_variable("BASE_CURRENCY")
         target_currencies = get_env_variable("TARGET_CURRENCIES")
         resolution = get_env_variable("RESOLUTION")
@@ -75,7 +87,7 @@ if __name__ == "__main__":
         places = get_env_variable("PLACES")
         format_ = get_env_variable("FORMAT")
 
-        print("API_KEY:", api_key)
+        print("API_KEY:", masked_api_key)
         print("START_DATE:", start_date)
         print("END_DATE:", end_date)
         print("BASE_CURRENCY:", base_currency)
@@ -85,10 +97,11 @@ if __name__ == "__main__":
         print("PLACES:", places)
         print("FORMAT:", format_)
 
-        api_key, start_date, end_date, base_currency, target_currencies, resolution, amount, places, format_ = get_user_input()
-        if None in (api_key, start_date, end_date, base_currency, target_currencies, resolution, format_):
+        if None in (masked_api_key, start_date, end_date, base_currency, target_currencies, resolution, format_):
             raise ValueError("One or more environment variables are not set.")
-        time_series_data = fetch_time_series_data(api_key, start_date, end_date, base_currency, target_currencies,
+
+        time_series_data = fetch_time_series_data(masked_api_key, start_date, end_date, base_currency,
+                                                  target_currencies,
                                                   resolution, amount, places, format_)
         if time_series_data:
             parse_time_series_data(time_series_data, start_date, end_date)
